03ba730cbd74e891bb39360b8212dd8533d40079
Ben Doyle <benjamin.peter.doyle@gmail.com>
2015-10-19 08:33:29 -0400

    Remove dead code

--- src/client/default.nix
+++ src/client/default.nix
@@ -1,4 +1,3 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghcjs" }:
 let callPackage = nixpkgs.pkgs.haskell.packages.${compiler}.callPackage;
-    paisley-core = callPackage ../core { compiler = "ghcjs"; };
 in callPackage ./paisley-client.nix { }
